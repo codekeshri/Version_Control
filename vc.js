8b2a4b19517c1823fb086ac8a19fde9b2b315009
codekeshri <arvidce@gmail.com>
2024-10-27 01:14:51 +0530

    ok

--- vc.js
+++ vc.js
@@ -1,4 +1,7 @@
-console.log('testing advanced git')
 console.log('I am writing a beautiful code')
+console.log('testing advanced git')
+
 
 console.log('merge dev with main')
+
+console.log('come onto my garden')
